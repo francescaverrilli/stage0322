4d2b9847134be2c87bf36424f7a9aa6b287e7e25
Luca Rianna <luca.rianna@soprasteria.com>
2022-03-08 15:15:56 +0100

    modifica eclipse

---
+++ 0803/stageProject/src/main/java/it/sopra/git/PrintName/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package it.sopra.git.PrintName.java;
+
+public class HelloWorld {
+	public class Helloworld {
+		 public static void main(String[]args) {
+			 System.out.println("Hello world");
+		 }
+		}
+
+}
